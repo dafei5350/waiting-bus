ca934e7ea532717e3ecd320d63e40f3773204a44
weiliang <535027650@qq.com>
2021-08-25 18:39:26 +0800

    地图

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
 
 const config = require('./config/index')
-<<<<<<< HEAD
-=======
-var QQMapWX = require('../../libs/qqmap-wx-jssdk.min');
->>>>>>> 9cb8d95 (sdk)
+// var QQMapWX = require('../../libs/qqmap-wx-jssdk.min');
 
 App({
   onLaunch() {
-    console.log(config.app);
-    // 展示本地存储能力
-<<<<<<< HEAD
-
-
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
+    wx.getSystemInfo({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        this.globalData.statusBar = res.statusBarHeight;
       }
     })
   },
-  globalData: {
-    userInfo: null
-=======
-    
 
-
-  },
   globalData: {
-    latitude: 34.75,
-    longitude: 113.66,
->>>>>>> 9cb8d95 (sdk)
+    statusBar: null,
+    initLatitude: null,
+    initLongitude: null,
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,69 +1,34 @@
-<<<<<<< HEAD
-// index.js
-// 获取应用实例
 const app = getApp()
-
-Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
-=======
-
-const app = getApp()
+import key from '../../config/index'
 var QQMapWX = require('../../libs/qqmap-wx-jssdk.min');
+
+var qqmapsdk
+var minOffset = 30;
+var minTime = 60;
+var startX = 0;
+var startY = 0;
+var startTime = 0;
+
 Page({
   data: {
-    latitude: app.globalData.latitude,
-    longitude: app.globalData.longitude,
+    latitude: 34.75,
+    longitude: 113.66,
+    statusBar: null,
+    height: '40%',
+    city: null,
   },
 
   onLoad() {
-    var qqmapsdk = new QQMapWX({
+    this.getLocation()
+    qqmapsdk = new QQMapWX({
       key: 'QE3BZ-R7ICD-ZIS42-HCM3D-N2GY5-CZFWQ'
     });
+    this.setData({
+      statusBar: app.globalData.statusBar
+    })
+
   },
   onShow: function () {
-    // 调用接口
     qqmapsdk.search({
       keyword: '华强',
       success: function (res) {
@@ -76,6 +41,79 @@ Page({
         console.log(res);
       }
     });
-  }
+  },
+  getLocation() {
+    let that = this
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        console.log(res);
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          speed: res.speed,
+        })
+        that.getCity(res.latitude, res.longitude)
+      }
+    })
+  },
+  getCity(latitude, longitude){
+    let that = this
+    qqmapsdk.reverseGeocoder({
+      sig: key.sig,
+      location: {latitude, longitude} || '',
+      // location: '35.978283,115.86075',
+      success(res){ 
+        console.log("城市",res);
+        console.log(res.result.address_component.district);
+        that.setData({
+          city: res.result.address_component.district
+        })
+      },
+      fail(err) {
+        console.log("获取城市失败", err);
+      }
+    })
+  },
+  touchStart(e) {
+    startX = e.touches[0].pageX;
+    startY = e.touches[0].pageY;
+    startTime = new Date().getTime();
+  },
+  touchCancel(e) {
+    startX = 0;
+    startY = 0;
+    startTime = 0;
+  },
+  touchEnd(e) {
+    let that = this
+    var endX = e.changedTouches[0].pageX;
+    var endY = e.changedTouches[0].pageY;
+    var touchTime = new Date().getTime() - startTime;
+    if (touchTime >= minTime) {
+      var xOffset = endX - startX;
+      var yOffset = endY - startY;
+      if (Math.abs(xOffset) >= Math.abs(yOffset) && Math.abs(xOffset) >= minOffset) {
+        if (xOffset < 0) {
+          console.log('向左滑动')
+        } else {
+          console.log('向右滑动')
+        }
+      } else if (Math.abs(xOffset) < Math.abs(yOffset) && Math.abs(yOffset) >= minOffset) {
+        if (yOffset < 0) {
+          console.log('向上滑动')
+          that.setData({
+            height: '85%'
+          })
+        } else {
+          console.log('向下滑动')
+          that.setData({
+            height: '40%'
+          })
+        }
+      }
+    } else {
+      console.log('滑动时间过短', touchTime)
+    }
+  },
 })
->>>>>>> 9cb8d95 (sdk)
